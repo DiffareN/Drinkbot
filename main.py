06c6261ac1fa15efc48ee9caa618556d4c09b6d7
DiffareN <sandstrom@groggy.se>
2016-09-08 14:45:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,39 @@
 ##############################################################
 from tkinter import *
 from drinkar.py import *
-
 	
 def sel():
-	selection = "Du har valt " + str(var.get()) + " cl"
+	if str(drink.get()) == '1':
+		drinknamn = "Redbull & vodka"
+	elif str(drink.get()) == '2':
+		drinknamn = "Sangria"
+	elif str(drink.get()) == '3':
+		drinknamn = "Tequila sunrise"
+	else:
+		drinknamn = "Du har inte valt en drink!"
+		
+	selection = "Du har valt " + str(var.get()) + " cl och " + drinknamn
 	label.config(text = selection)
 
 root = Tk()
+
+drink = IntVar()
+D1 = Radiobutton(root, text="Redbull & Vodka", indicatoron=0, width=25, variable=drink, value=1,
+                  command=sel)
+D1.pack( side = LEFT )
+
+D2 = Radiobutton(root, text="Sangria", indicatoron=0, width=25, variable=drink, value=2,
+                  command=sel)
+D2.pack( side = LEFT )
+
+D3 = Radiobutton(root, text="Tequila sunrise", indicatoron=0, width=25, variable=drink, value=3,
+                  command=sel)
+D3.pack( side = LEFT )
+
+
+#HÄR SKA VA RADBRYTNING
+print ("Välj styrka på drinken:")
+
 var = IntVar()
 R1 = Radiobutton(root, text="4 cl", indicatoron=0, width=10, variable=var, value=4,
                   command=sel)
