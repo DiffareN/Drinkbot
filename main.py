8db6bdf21a25ca9ed9851f6c3b79ca3e5ec4afc6
DiffareN <sandstrom@groggy.se>
2016-09-08 17:28:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 # Drinkbot by Fredrik "Diffe" Sandstrom and Rikard Johansson #
 # Feel free to use the code as long as this text stays intact#
 ##############################################################
-##############################################################
-# Drinkbot by Fredrik "Diffe" Sandstrom and Rikard Johansson #
-# Feel free to use the code as long as this text stays intact#
-##############################################################
 from tkinter import *
 from drinkar import * 
 	
@@ -25,15 +21,15 @@ root = Tk()
 drink = IntVar()
 D1 = Radiobutton(root, text="Redbull & Vodka", indicatoron=0, width=25, variable=drink, value=1,
                   command=sel)
-D1.place(x=30, y=30)
+D1.place(x=50, y=30)
 
 D2 = Radiobutton(root, text="Sangria", indicatoron=0, width=25, variable=drink, value=2,
                   command=sel)
-D2.place(x=280, y=30)
+D2.place(x=300, y=30)
 
 D3 = Radiobutton(root, text="Tequila sunrise", indicatoron=0, width=25, variable=drink, value=3,
                   command=sel)
-D3.place(x=530, y=30)
+D3.place(x=550, y=30)
 
 
 
@@ -43,21 +39,21 @@ D3.place(x=530, y=30)
 var = IntVar()
 R1 = Radiobutton(root, text="4 cl", indicatoron=0, width=10, variable=var, value=4,
                   command=sel)
-R1.pack( side = LEFT )
+R1.place(x=135, y=300)
 
 R2 = Radiobutton(root, text="6 cl", indicatoron=0, width=10, variable=var, value=6,
                   command=sel)
-R2.pack( side = LEFT )
+R2.place(x=335, y=300)
 
 R3 = Radiobutton(root, text="8 cl", indicatoron=0, width=10, variable=var, value=8,
                   command=sel)
-R3.pack( side = LEFT )
+R3.place(x=535, y=300)
 
 label = Label(root)
-label.place(x=295, y=0)
-label.configure(background='white')
+label.place(x=285, y=0)
+label.configure(background='#533B9D')
 root.overrideredirect(1)
-root.configure(background='white')
+root.configure(background='#533B9D')
 root.geometry("800x480")
 root.bind("<Escape>", lambda e: e.widget.quit())
 root.mainloop()
