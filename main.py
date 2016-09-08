623970965432d97bfe127d39b025bbf97baf4069
DiffareN <sandstrom@groggy.se>
2016-09-08 12:56:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-# Drinkbot by Fredrik "Diffe" Sandstrom and Rikard Johansson
-# Feel free to use the code as long as this text stays intact
+##############################################################
+# Drinkbot by Fredrik "Diffe" Sandstrom and Rikard Johansson #
+# Feel free to use the code as long as this text stays intact#
+##############################################################
+from tkinter import *
 
-!# /us/bin/python
+def sel():
+   selection = "Du har valt " + str(var.get()) + " cl"
+   label.config(text = selection)
+
+root = Tk()
+var = IntVar()
+R1 = Radiobutton(root, text="4 cl", indicatoron=0, variable=var, value=4, command=sel)
+R1.pack( anchor = W )
+
+R2 = Radiobutton(root, text="6 cl", indicatoron=0, variable=var, value=6, command=sel)
+R2.pack( anchor = W )
+
+R3 = Radiobutton(root, text="8 cl", indicatoron=0, variable=var, value=8, command=sel)
+R3.pack( anchor = W)
+
+label = Label(root)
+label.pack()
+root.mainloop()
