0d86c730eeb3508b96a0a34ce855a850a8418de9
DiffareN <sandstrom@groggy.se>
2016-09-08 14:21:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,30 @@
 ##############################################################
 from tkinter import *
 
+
+	
 def sel():
-   selection = "Du har valt " + str(var.get()) + " cl"
-   label.config(text = selection)
+	selection = "Du har valt " + str(var.get()) + " cl"
+	label.config(text = selection)
 
 root = Tk()
 var = IntVar()
-R1 = Radiobutton(root, text="4 cl", indicatoron=0, variable=var, value=4, command=sel)
-R1.pack( anchor = W )
+R1 = Radiobutton(root, text="4 cl", indicatoron=0, width=10, variable=var, value=4,
+                  command=sel)
+R1.pack( side = LEFT )
 
-R2 = Radiobutton(root, text="6 cl", indicatoron=0, variable=var, value=6, command=sel)
-R2.pack( anchor = W )
+R2 = Radiobutton(root, text="6 cl", indicatoron=0, width=10, variable=var, value=6,
+                  command=sel)
+R2.pack( side = LEFT )
 
-R3 = Radiobutton(root, text="8 cl", indicatoron=0, variable=var, value=8, command=sel)
-R3.pack( anchor = W)
+R3 = Radiobutton(root, text="8 cl", indicatoron=0, width=10, variable=var, value=8,
+                  command=sel)
+R3.pack( side = LEFT )
 
 label = Label(root)
 label.pack()
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+root.overrideredirect(1)
+root.geometry("%dx%d+0+0" % (w, h))
+root.bind("<Escape>", lambda e: e.widget.quit())
 root.mainloop()
